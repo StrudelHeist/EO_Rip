22ba797ce52a9cd9b24ea39ca46c12e3f53588b4
StrudelHeist <jacob.llaneras@portsolutionintegrators.com>
2021-07-07 15:07:48 -0400

    Skeleton code
    Added some razor pages and starting to add data for characters

---
+++ EtrianOdysseyClone/Data/GameSession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EtrianOdysseyClone.Data
+{
+    public class GameSession
+    {
+        public int Monies { get; set; }
+        
+        // TODO: Party
+
+        // TODO: Map data
+
+        // TODO: Quest data
+    }
+}
---
+++ EtrianOdysseyClone/Data/Jobs/IJob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EtrianOdysseyClone.Data.Jobs
+{
+    public interface IJob
+    {
+        // TODO: List of skills for job
+        // TODO: Level up method (different classes/jobs should level up differently)
+        // TODO: Base stats per job
+    }
+}
