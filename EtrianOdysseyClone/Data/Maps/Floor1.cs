9a5e43e04cbb19ff90ac7641580d8c017d8c6c5e
StrudelHeist <jacob.llaneras@portsolutionintegrators.com>
2021-07-08 14:24:24 -0400

    Continuing map work

---
+++ EtrianOdysseyClone/Data/Maps/Floor1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace EtrianOdysseyClone.Data.Maps
+{
+    public class Floor1
+    {
+        public List<MapCell> MapLayout { get; set; }
+
+        public Floor1()
+        {
+            MapLayout = new List<MapCell>();
+
+            // TODO: Build map from text file
+        }
+
+        public void DigestMapFile(string pathToMapFile)
+        {
+            using (StreamReader mapFile = new StreamReader(pathToMapFile))
+            {
+                string line;
+                while ((line = mapFile.ReadLine()) != null)
+                {
+
+                }
+            }
+        }
+    }
+}
