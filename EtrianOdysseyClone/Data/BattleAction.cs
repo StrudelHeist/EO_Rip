369a6f15ca553e0a4decbb743dc8805abc276a2d
StrudelHeist <jacob.llaneras@portsolutionintegrators.com>
2021-07-12 12:37:43 -0400

    Battle actions

---
+++ EtrianOdysseyClone/Data/BattleAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace EtrianOdysseyClone.Data
+{
+    public class BattleAction
+    {
+        public int ActionSpeed { get; set; }
+
+        public delegate void TheAction();
+
+        public TheAction ActualAction { get; set; }
+
+        public string ActionInformation { get; set; }
+    }
+}
